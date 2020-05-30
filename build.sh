eb95a4bee678801a8780752164d2b74dc074cee6
bogo8liuk <lucaborghi99@gmail.com>
2020-05-31 01:39:08 +0200

    little fix to build.sh

--- build.sh
+++ build.sh
@@ -2,40 +2,24 @@
 
 set -e
 
-movida=0
-
-for file in *
-do
-    if [ -d ${file} ]
-    then
-        if [ ${file} == "movida" ]
-        then
-            movida=1
-            break
-        fi
-    fi
-done
-
-if [ $((movida)) -eq 0 ]
-then
-    return
-fi
-
 path="$(dirname `realpath "${BASH_SOURCE[0]}"`)"
 
 function undo_build {
-    read -p "You are deleting all the compiled files. Are you sure to continue [Y/n]?" user_in
+    read -p "You are deleting all the compiled files. Are you sure to continue [Y/n]? " -n 1 user_in
     case "${user_in}" in
-        "Y" | "y" | "\n")
+        "Y" | "y" | "")
+        echo ""
         rm ${path}/movida/borghicremona/*.class
         rm ${path}/movida/commons/*.class
         ;;
 
         "N" | "n")
+        echo ""
         return
         ;;
 
         *)
+        echo ""
         undo_build
         ;;
     esac
