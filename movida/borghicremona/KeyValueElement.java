4ac54fd02cc7072398f358a33548fd9234a55325
bogo8liuk <58339191+bogo8liuk@users.noreply.github.com>
2020-10-07 15:53:50 +0200

    Hash (#2)
    * build.sh new feature; Dictionary update
    
    * name update
    
    * changes and new module Test
    
    * final HashMap test
    
    * hash function made static

---
+++ movida/borghicremona/KeyValueElement.java
@@ -0,0 +1,20 @@
+package movida.borghicremona;
+
+public class KeyValueElement {
+    final protected Comparable key;
+    public Object data;
+
+    public KeyValueElement() {
+        this.key = null;
+        this.data = null;
+    }
+
+    public KeyValueElement(Comparable key, Object data) {
+        this.key = key;
+        this.data = data;
+    }
+
+    public Comparable getKey() {
+        return this.key;
+    }
+}
