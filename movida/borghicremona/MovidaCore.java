ad554c1c4eea2dc64b7c5453759acb0afbbc3579
bogo8liuk <lucaborghi99@gmail.com>
2020-11-14 18:09:17 +0100

    IMovida* implementation

--- movida/borghicremona/MovidaCore.java
+++ movida/borghicremona/MovidaCore.java
@@ -1,4 +1,81 @@
 package movida.borghicremona;
 
-public class MovidaCore {
+import movida.commons.*;
+
+public class MovidaCore implements IMovidaDB, IMovidaConfig, IMovidaSearch {
+	public boolean setSort(SortingAlgorithm algorithm) {
+
+	}
+
+	public boolean setMap(MapImplementation map) {
+
+	}
+
+	public void loadFromFile(File f) {
+
+	}
+
+	public void saveToFile() {
+		
+	}
+
+	public void clear() {
+
+	}
+
+	public int countMovies() {
+
+	}
+
+	public int countPeople() {
+
+	}
+
+	public boolean deleteMovieByTitle(String title) {
+
+	}
+
+	public Movie getMovieByTitle(String title) {
+
+	}
+
+	public Person getPersonByName(String name) {
+
+	}
+
+	public Movie[] getAllMovies() {
+
+	}
+
+	public Person[] getAllPeople() {
+
+	}
+
+	public Movie[] searchMoviesByTitle(String title) {
+
+	}
+
+	public Movie[] searchMoviesInYear(Integer year) {
+
+	}
+
+	public Movie[] searchMoviesDirectedBy(String name) {
+
+	}
+
+	public Movie[] searchMoviesStarredBy(String name) {
+
+	}
+
+	public Movie[] searchMostVotedMovies(Integer N) {
+
+	}
+
+	public Movie[] searchMostRecentMovies(Integer N) {
+
+	}
+
+	public Person[] searchMostActiveActors(Integer N) {
+
+	}
 }
