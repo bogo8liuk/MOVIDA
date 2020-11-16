5876be23cdded267f9b25d7626e0d54211030ebd
steelif <s.cremona89@gmail.com>
2020-11-16 11:59:30 +0100

    wip

--- movida/borghicremona/abr/Abr.java
+++ movida/borghicremona/abr/Abr.java
@@ -6,25 +6,25 @@ import java.lang;
 
 public class Abr implements Dictionary {
     private KeyValueElement nodeValue;
-    private Abr leftChild;
-    private Abr rightChild;
+    private Abr left;
+    private Abr right;
 
     public Abr(KeyValueElement nodeValue) {
         this.nodeValue = nodeValue;
-        this.leftChild = null;
-        this.rightChild = null;
+        this.left = null;
+        this.right = null;
     }
     //gestione valori uguali ?
 	// soluzione 1: decidiamo arbitrariamente di metterli sempre o a destra o a sinistra
 	// soluzione 2: decidiamo di aggiungere un contatore di fianco al valore del nodo, che indica quanti nodi hanno quel valore 
-    public void insert(KeyValueElement Value) {
+    public void insert(KeyValueElement value) {
         Abr node = root;
         while (null != node) {
             if (value.compareTo(node.nodeValue) < 0)
-                node = node.leftChild;
+                node = node.left;
             else {
                 if (value.compareTo(node.nodeValue()) > 0)
-                    node = node.rightChild;
+                    node = node.right;
                 else 
                     return;
                 }
@@ -37,17 +37,19 @@ public class Abr implements Dictionary {
         boolean found = false;
         while ((null != node) && (!found)) {
             if (value.compareTo(node.nodeValue) < 0)
-                node = node.leftChild;
+                node = node.left;
             else {
                 if (value.compareTo(Node.nodeValue) > 0)
-                    node = node.rightChild;
+                    node = node.right;
                 else
                     found = true;
             }
         }
         return found;
     }
-
+	
+	
+	
     public void delete(KeyValueElement value) throws TreeException {
     
     }
