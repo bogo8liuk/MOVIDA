03c53da17465878610af47b67c31db0a914137eb
bogo8liuk <lucaborghi99@gmail.com>
2020-11-21 11:54:13 +0100

    wip: MovidaCore

--- movida/borghicremona/MovidaCore.java
+++ movida/borghicremona/MovidaCore.java
@@ -1,14 +1,95 @@
 package movida.borghicremona;
 
 import movida.commons.*;
+import movida.borghicremona.hashmap;
+import movida.borghicremona.graph;
+import movida.borghicremona.sort;
 
 public class MovidaCore implements IMovidaDB, IMovidaConfig, IMovidaSearch, IMovidaCollaborations {
-	public boolean setSort(SortingAlgorithm algorithm) {
+	private MapImplementation dictionary;
+	private SortingAlgorithm algorithm;
+	private HashMap hashmap;
+	// TODO: abr
+	private NonOrientedGraph graph;
 
+	public MovidaCore() {
+		this.hashmap = null;
+		// TODO: abr
+		this.graph = null; // TODO: (?)
+		this.dictionary = null;
+		this.algorithm = null;
+	}
+
+	/**
+	 * It sets the value of algorithm member field to the value passed as parameter,
+	 * if possible.
+	 *
+	 * @param algorithm Value to set.
+	 *
+	 * @return true If the setting is successful, false otherwise.
+	 */
+	private boolean __setSort(SortingAlgorithm algorithm) {
+		switch (algorithm) {
+			case SelectionSort:
+			case QuickSort:
+				this.algorithm = algorithm;
+				break;
+
+			default:
+				return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * It sets the value of dictionary member field to the value passed as parameter,
+	 * if possibile.
+	 *
+	 * @param map Value to set.
+	 *
+	 * @return true If the setting is successful, false otherwise.
+	 */
+	private boolean __setMap(MapImplementation map) {
+		switch (map) {
+			case ABR:
+			case HashIndirizzamentoAperto:
+				this.dictionary = map;
+				break;
+
+			default:
+				return false;
+		}
+
+		return true;
+	}
+
+	public MovidaCore(SortingAlgorithm algorithm, MapImplementation map) {
+		try {
+			if (!__setSort(algorithm) || !__setMap(map))
+				throw new IllegalArgumentException("Impossible to allocate a MovidaCore instance");
+		} catch(IllegalArgumentException exception) {
+			System.err.println(exception.getMessage());
+			System.exit(-1);
+		}
+
+		this.hashmap = null;
+		// TODO: abr
+		this.graph = null; // TODO: (?)
+	}
+
+	public boolean setSort(SortingAlgorithm algorithm) {
+		if (null != this.algorithm)
+			return false;
+
+		return __setSort(algorithm);
 	}
 
 	public boolean setMap(MapImplementation map) {
+		if (null != this.dictionary)
+			return false;
 
+		return __setMap(map);
 	}
 
 	public void loadFromFile(File f) {
